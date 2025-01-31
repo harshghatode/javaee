1a1ca59143259b12a9ce496a8629baef57897850
harshghatode <harshghatode1000@gmail.com>
2025-01-31 22:28:38 +0530

    commit to dev

---
+++ firstproject/src/com/wipro/java/harsh.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.wipro.java;
+
+/**
+ * 
+ */
+public class harsh {
+
+	/**
+	 * 
+	 */
+	public harsh() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my first program");
+
+	}
+
+}
